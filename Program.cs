fef6354c53d308db09dde9166cccafaab26fd351
Chendy Lundy Janitra Putra Ashari <chendy.lundy.ft21@mail.umy.ac.id>
2022-10-19 16:47:26 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+
+
+
+namespace Quicsort
+{
+    class Program
+    {
+        private int[] arr = new int[20];
+        private int cmp_count = 0;
+        private int mov_count = 0;
+
+        private int n;
+
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter The Number Of Elements in The Array :");
+     
+            }
+        }
+    }
+}
