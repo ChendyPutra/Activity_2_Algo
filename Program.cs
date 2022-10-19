c7707c9af6ad6091a839333664c7bc4547e3111e
Chendy Lundy Janitra Putra Ashari <chendy.lundy.ft21@mail.umy.ac.id>
2022-10-19 17:17:21 +0700

    Membuat method input yang berisi inputan bagi element

--- Program.cs
+++ Program.cs
@@ -14,5 +14,28 @@ namespace Quicsort
 
         private int n;
 
+        void input()
+        {
+            while (true)
+            {
+                Console.Write("Enter The Number Of Elements In The Array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\nArray can Have Maximum 20 Elements \n");
+            }
+            Console.WriteLine("\n==================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n==================");
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s = Console.ReadLine();
+                arr[i] = Int32.Parse(s1);
+            }
+        }
     }
 }
